5a4fbe4ae8e553c62726031163af6bbd6ff8873d
Phil Bouchard <philippeb8@gmail.com>
2022-08-10 12:25:52 -0400

    Made the range generic.

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ for key in galaxies:
     shortestdiff = 9e99
     shortestvelocities = []
     
-    for h in [1e17, 5e17, 1e18, 5e18, 1e19, 5e19, 1e20, 5e20]:
+    for h in np.arange(1e19, 5e20, 1e19):
         ftvelocities = [h/(m/x+h)*math.sqrt(G*m/x) for x in xvelocities]
         w = (yvelocities[-1] - ftvelocities[-1]) / xvelocities[-1]
         ft2velocities = [y + w * x for (x, y) in zip(xvelocities, ftvelocities)]
@@ -90,7 +90,7 @@ for key in galaxies:
     sig_ff.append([xvelocities, shortestvelocities])
 
     plt.clf()
-    plt.title(key + " Rotation Curve's Tangential Velocity vs Radius (h = " + str(shortesth) + ", w = " + str(shortestw) + ")")
+    plt.title(key + " Rotation Curve vs Radius (h = " + str(shortesth) + ", w = " + str(shortestw) + ")")
 
     plt.plot(sig_ff[0][0], sig_ff[0][1], label='Observed Data')
     plt.plot(sig_ff[1][0], sig_ff[1][1], label='Theoretical Data')
